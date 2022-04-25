1eff95efa06ad1db8ab5bd4b0c09848c25527327
Kai <kai@v2ex.com>
2022-04-25 16:35:44 +0800

    Add new data store to handle project loading / saving tasks.

--- Launcher/Model/CLStore.swift
+++ Launcher/Model/CLStore.swift
@@ -10,8 +10,40 @@ import Combine
 import Foundation
 import SwiftUI
 
+
+private actor CLDataStore {
+    func saveProjects(_ projects: [CLProject]) {
+        let databasePath = CLTaskManager.shared.databasePath()
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .withoutEscapingSlashes
+        do {
+            let data = try encoder.encode(projects)
+            try data.write(to: databasePath, options: .atomic)
+        } catch {
+            debugPrint("failed to save tasks: \(error)")
+        }
+    }
+
+    func loadProjects() -> [CLProject] {
+        let databasePath = CLTaskManager.shared.databasePath()
+        let decoder = JSONDecoder()
+        do {
+            let data = try Data(contentsOf: databasePath)
+            return try decoder.decode([CLProject].self, from: data).sorted { a, b in
+                a.created > b.created
+            }
+        } catch {
+            debugPrint("failed to load tasks: \(error).")
+        }
+        return []
+    }
+}
+
+
 class CLStore: ObservableObject {
     static let shared: CLStore = .init()
+
+    private let store = CLDataStore()
 
     var envSHELL: String = ""
     var envPATH: String = ""
@@ -20,8 +52,8 @@ class CLStore: ObservableObject {
 
     @Published var projects: [CLProject] = [] {
         didSet {
-            DispatchQueue.global(qos: .background).async {
-                self._saveTasks()
+            Task.detached {
+                await self.store.saveProjects(self.projects)
             }
             if let uuidString = CLDefaults.default.lastActiveProjectUUID, let uuid = UUID(uuidString: uuidString) {
                 DispatchQueue.main.async {
@@ -123,14 +155,13 @@ class CLStore: ObservableObject {
 
     init() {
         debugPrint("CL Store Init.")
-        let p = _loadTasks().sorted { a, b in
-            a.created > b.created
-        }
-        DispatchQueue.main.async {
-            self.projects = p
-            DispatchQueue.global(qos: .utility).async {
-                self._detectExistingTasks()
+
+        Task.init {
+            let p = await self.store.loadProjects()
+            await MainActor.run {
+                self.projects = p
             }
+            self._detectExistingTasks()
         }
     }
 
@@ -169,52 +200,20 @@ class CLStore: ObservableObject {
             return p
         }
 
-        DispatchQueue.global(qos: .background).async {
-            self._saveTasks()
-        }
-
         if let img = CLTaskManager.shared.projectAvatar(projectID: editingProject.id, isEditing: true) {
             CLTaskManager.shared.updateProjectAvatar(image: img)
             CLTaskManager.shared.removeProjectAvatar(projectID: editingProject.id, isEditing: true)
         }
         NotificationCenter.default.post(name: .updateProjectAvatar, object: nil)
-    }
 
-    private func _saveTasks() {
-        let databasePath = CLTaskManager.shared.databasePath()
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .withoutEscapingSlashes
-        do {
-            let data = try encoder.encode(projects)
-            try data.write(to: databasePath, options: .atomic)
-        } catch {
-            debugPrint("failed to save tasks: \(error)")
+        let projectsToSave = projects
+        Task.detached {
+            await self.store.saveProjects(projectsToSave)
         }
-    }
-
-    private func _loadTasks() -> [CLProject] {
-        let databasePath = CLTaskManager.shared.databasePath()
-        let decoder = JSONDecoder()
-        do {
-            let data = try Data(contentsOf: databasePath)
-            return try decoder.decode([CLProject].self, from: data)
-        } catch {
-            debugPrint("failed to load tasks: \(error), try to reset to default.")
-            if FileManager.default.fileExists(atPath: databasePath.path) {
-                try? FileManager.default.removeItem(at: databasePath)
-                DispatchQueue.global(qos: .utility).async {
-                    DispatchQueue.main.async {
-                        self.projects = self._loadTasks().sorted { a, b in
-                            a.created > b.created
-                        }
-                    }
-                }
-            }
-        }
-        return []
     }
 
     private func _detectExistingTasks() {
+        debugPrint("detecting tasks...")
         let taskPath = CLTaskManager.shared.taskPath()
         let decoder = JSONDecoder()
         do {
